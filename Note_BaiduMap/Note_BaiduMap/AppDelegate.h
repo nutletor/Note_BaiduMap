b35d9f689ce346744e1e4fee04e7f9677130580c
Huangfei <huangfei_07@163.com>
2016-02-26 19:04:55 +0800

    initial commit

---
+++ Note_BaiduMap/Note_BaiduMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Note_BaiduMap
+//
+//  Created by THN-Huangfei on 16/2/26.
+//  Copyright © 2016年 Huangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Note_BaiduMap/Note_BaiduMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Note_BaiduMap
+//
+//  Created by THN-Huangfei on 16/2/26.
+//  Copyright © 2016年 Huangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
